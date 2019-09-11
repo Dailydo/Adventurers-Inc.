74e5e66a9c0b92e9c1abf40607f2d9956a637f8f
DESKTOP-4VP0SSO\Dailydo <dailydo_31@hotmail.fr>
2019-09-12 00:03:14 +0200

    Created scene and basic character controller

---
+++ Adventurers-Inc/Assets/Scripts/Character.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Character : MonoBehaviour
+{
+    public string _name = "John Doe";       //Character's name
+    public string[] _nameCandidates;
+
+    private TextMeshPro _3dText;            //Reference to the TextMeshPro script in the 3dName game object
+
+
+    void Awake()
+    {
+        _3dText = transform.Find("3dName").GetComponent<TextMeshPro>();
+        Set3dName(_name);
+    }
+
+
+    //Sets the character name (variable + 3d display) on "newName"
+    void Set3dName (string newName)
+    {
+        _name = newName;
+        _3dText.text = newName;
+    }
+
+    //Assigns a random name to the character
+    public void AssignRandomName()
+    {
+        string currentName = _name;
+        int maxIndex = _nameCandidates.Length;
+        string newName = currentName;
+
+        while (newName == currentName)
+        {
+            newName = _nameCandidates[Random.Range(0, maxIndex)];
+        }
+        //Randomization part to be truly implemented later
+
+        _name = newName;
+        Set3dName(newName);
+    }
+}
---
+++ Adventurers-Inc/Assets/Scripts/CharacterBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBody : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit) && (hit.transform.tag == "Character"))
+            {
+                transform.parent.GetComponent<Character>().AssignRandomName();
+            }
+        }
+    }
+}
